ad57d4741f1d2e56541189e6ceaa4c828349506c
Volodymyr <voloda99911@gmail.com>
2022-02-20 14:52:34 +0200

    Змінено інтерфейс розпочато роботу з розміткою вікна First Post

---
+++ terminalUSB/terminalUSB/ViewModels/Base/ViewModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace terminalUSB.ViewModels.Base
+{
+    internal abstract class ViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string PropertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+        }
+
+        protected virtual bool Set<T>(ref T field, T value, [CallerMemberName] string PropertyName = null)
+        {
+            if (Equals(field, value)) return false;
+            field = value;
+            OnPropertyChanged(PropertyName);
+            return true;
+        }
+
+        public void Disposed()
+        {
+            Disposed(true);
+        }
+
+        private bool _Disposed;
+
+        protected virtual void Disposed(bool Disposing)
+        {
+            if (!Disposing || _Disposed) return;
+            {
+                _Disposed = true;
+            }
+        }
+    }
+}
---
+++ terminalUSB/terminalUSB/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using terminalUSB.ViewModels.Base;
+
+namespace terminalUSB.ViewModels
+{
+    internal class MainWindowViewModel : ViewModel
+    {
+        private string _Title = "Terminal";
+
+        public string Title
+        {
+            get => _Title;
+            set => Set(ref _Title, value);
+        }
+
+    }
+}
