ab5241ff3ef2ee643f4a500446c58befe943fa54
Volodymyr <voloda99911@gmail.com>
2022-03-02 22:59:47 +0200

    створено клас команди Виключення

---
+++ terminalUSB/terminalUSB/Infafstructure/Commands/CloseApplicationCommand.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Windows;
+using terminalUSB.Infafstructure.Commands.Base;
+
+namespace terminalUSB.Infafstructure.Commands
+{
+    internal class CloseApplicationCommand : Command
+    {
+
+        public override bool CanExecute(object parameter) => true;
+
+        public override void Execute(object parameter) => Application.Current.Shutdown();
+    }
+}
