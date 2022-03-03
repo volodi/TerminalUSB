54d2358609c7c4d5edfb8d81e809177e53072254
Volodymyr <voloda99911@gmail.com>
2022-03-03 18:29:23 +0200

    додано модель та представлення для графіка

---
+++ terminalUSB/terminalUSB/Models/DataPoint.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace terminalUSB.Models
+{
+    internal class DataPoint
+    {
+        public double XValue { get; set; }
+
+        public double YValue { get; set; }
+    }
+}
--- terminalUSB/terminalUSB/ViewModels/MainWindowViewModel.cs
+++ terminalUSB/terminalUSB/ViewModels/MainWindowViewModel.cs
@@ -4,6 +4,7 @@ using System.Text;
 using System.Windows;
 using System.Windows.Input;
 using terminalUSB.Infafstructure.Commands;
+using terminalUSB.Models;
 using terminalUSB.ViewModels.Base;
 
 namespace terminalUSB.ViewModels
@@ -18,6 +19,18 @@ namespace terminalUSB.ViewModels
             set => Set(ref _Title, value);
         }
 
+        #region
+        /// <summary>
+        /// </summary>
+        private IEnumerable<DataPoint> _DataPointUART;
+
+        public IEnumerable<DataPoint> DataPointUART
+        {
+            get => _DataPointUART;
+            set => Set(ref _DataPointUART, value);
+        }
+
+        #endregion
 
         #region Status
 
@@ -47,7 +60,21 @@ namespace terminalUSB.ViewModels
 
         public MainWindowViewModel()
         {
+            #region Command
             ClaseApplicationCommand = new LambdaCommand(OnClaseApplicationCommand, CanClaseApplicationCommand);
+            #endregion
+
+
+            var data_points = new List<DataPoint>((int)(360 / 0.1));
+            for (var x = 0d; x <= 360;x+=0.1)
+            {
+                const double to_rad = Math.PI / 180;
+                var y = Math.Sin(x * to_rad);
+
+                data_points.Add(new DataPoint { XValue = x, YValue = y });
+            }
+
+            DataPointUART = data_points;
         }
     }
 }
